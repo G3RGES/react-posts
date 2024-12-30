8f84c1b991919eb1e5ccf4f1b3181900076d0e2c
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-30 15:35:03 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <main>
+      <Post author="Gerges" body="Welcome to Reactjs Posts" />
+      <Post author="Georges" body="Check out my posts" />
+    </main>
+  );
+}
+
+export default App;

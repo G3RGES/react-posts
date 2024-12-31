f09400d0a3d64de307fa65bde4d195c713f766da
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-31 12:52:09 +0200

    Update PostsList.jsx
    using useState and passing the set method to the newPost component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 
-const PostsList = (props) => {
+const PostsList = () => {
+  const [bodyText, setBodyText] = useState("");
+
   return (
     <>
-      <NewPost />
+      <NewPost setBodyText={setBodyText} />
       <ul className={classes.posts}>
+        <Post author="Gerges" body={bodyText} />
         <Post author="Gerges" body="Welcome to Reactjs Posts" />
         <Post author="Georges" body="Check out my posts" />
       </ul>

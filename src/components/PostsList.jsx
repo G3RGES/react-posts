aea618b37eb2c463f690b7c331cb0ed7db210eb5
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-31 13:02:08 +0200

    Update PostsList.jsx

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,10 +5,15 @@ import NewPost from "./NewPost";
 
 const PostsList = () => {
   const [bodyText, setBodyText] = useState("");
+  const [authorName, setAuthorName] = useState("");
 
   function changeBodyHandler(event) {
     // console.log(event.target.value); //* TESTING
     setBodyText(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setAuthorName(event.target.value);
   }
 
   return (
@@ -16,9 +21,10 @@ const PostsList = () => {
       <NewPost
         setBodyText={setBodyText}
         changeBodyHandler={changeBodyHandler}
+        changeAuthorHandler={changeAuthorHandler}
       />
       <ul className={classes.posts}>
-        <Post author="Gerges" body={bodyText} />
+        <Post author={authorName} body={bodyText} />
         <Post author="Gerges" body="Welcome to Reactjs Posts" />
         <Post author="Georges" body="Check out my posts" />
       </ul>

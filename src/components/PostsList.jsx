dd96287f4a03632fb09cae27b8d914dd7c894224
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-31 12:23:26 +0200

    Update PostsList.jsx

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import NewPost from "./NewPost";
 
 const PostsList = (props) => {
   return (
     <>
-      <PostsList />
+      <NewPost />
       <ul className={classes.posts}>
         <Post author="Gerges" body="Welcome to Reactjs Posts" />
         <Post author="Georges" body="Check out my posts" />

70144896fd59f48584a9a3ce8af60cb6bcfc1070
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-12-31 12:46:20 +0200

    Update NewPost.jsx

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
 function NewPost() {
-  function changeBodyHandler() {}
+  const [bodyText, setBodyText] = useState("");
+
+  function changeBodyHandler(event) {
+    // console.log(event.target.value); //* TESTING
+    setBodyText(event.target.value);
+  }
 
   return (
     <form className={classes.form}>
@@ -9,6 +15,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{bodyText}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />

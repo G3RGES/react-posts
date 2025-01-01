8e27fdb848989776cedb652571756da8aed0e77e
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-01-01 16:06:35 +0200

    Update NewPost.jsx

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost({ setBodyText, changeBodyHandler, changeAuthorHandler }) {
+function NewPost({
+  setBodyText,
+  changeBodyHandler,
+  changeAuthorHandler,
+  closeModal,
+}) {
   // const [bodyText, setBodyText] = useState("");
 
   // function changeBodyHandler(event) {
@@ -19,6 +24,13 @@ function NewPost({ setBodyText, changeBodyHandler, changeAuthorHandler }) {
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required onChange={changeAuthorHandler} />
+      </p>
+
+      <p className={classes.actions}>
+        <button type="button" onClick={closeModal}>
+          Cancel
+        </button>
+        <button>Submit</button>
       </p>
     </form>
   );

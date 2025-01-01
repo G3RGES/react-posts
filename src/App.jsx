9b5175a1d53067a800453fa87b8bb5cba2b5a9a3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-01-01 15:47:37 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import MainHeader from "./components/MainHeader";
 import Post from "./components/Post";
 import PostsList from "./components/PostsList";
 
 function App() {
   return (
     <main>
+      <MainHeader />
       <PostsList />
     </main>
   );

6e9ffdb6ac05bbfd4d4310e3c1409a04869e1417
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-01-01 15:56:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import MainHeader from "./components/MainHeader";
 import Post from "./components/Post";
 import PostsList from "./components/PostsList";
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+
+  function showModal() {
+    setModalIsVisible(true);
+  }
+
+  function hideModal() {
+    setModalIsVisible(false);
+  }
+
   return (
-    <main>
-      <MainHeader />
-      <PostsList />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModal} />
+      <main>
+        <PostsList modalVisible={modalIsVisible} hideModal={hideModal} />
+      </main>
+    </>
   );
 }
 

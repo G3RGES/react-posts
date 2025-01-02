0976a2ecb1b4b4b0c228597680ca4361771eb84e
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-01-02 11:56:16 +0200

    Update PostsList.jsx

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,33 +5,20 @@ import NewPost from "./NewPost";
 import Modal from "./Modal";
 
 const PostsList = ({ modalVisible, hideModal }) => {
-  const [bodyText, setBodyText] = useState("");
-  const [authorName, setAuthorName] = useState("");
-
-  function changeBodyHandler(event) {
-    // console.log(event.target.value); //* TESTING
-    setBodyText(event.target.value);
-  }
-
-  function changeAuthorHandler(event) {
-    setAuthorName(event.target.value);
-  }
-
   return (
     <>
       {modalVisible === true && (
         <Modal hideModal={hideModal}>
           <NewPost
-            setBodyText={setBodyText}
-            changeBodyHandler={changeBodyHandler}
-            changeAuthorHandler={changeAuthorHandler}
+            // changeBodyHandler={changeBodyHandler}
+            // changeAuthorHandler={changeAuthorHandler}
             closeModal={hideModal}
           />
         </Modal>
       )}
 
       <ul className={classes.posts}>
-        <Post author={authorName} body={bodyText} />
+        {/* <Post author={authorName} body={bodyText} /> */}
         <Post author="Gerges" body="Welcome to Reactjs Posts" />
         <Post author="Georges" body="Check out my posts" />
       </ul>

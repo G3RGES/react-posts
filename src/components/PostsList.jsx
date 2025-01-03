495ff6ea234e45559d685e0c004856f6cecd761d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-01-03 12:11:28 +0200

    Update PostsList.jsx

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
@@ -6,6 +6,19 @@ import Modal from "./Modal";
 
 const PostsList = ({ modalVisible, hideModal }) => {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function getPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const data = await response.json();
+      setPosts(data.posts);
+      setIsFetching(false);
+    }
+
+    getPosts();
+  }, []);
 
   function addPostsHandler(postsData) {
     fetch("http://localhost:8080/posts", {
@@ -32,7 +45,7 @@ const PostsList = ({ modalVisible, hideModal }) => {
         </Modal>
       )}
 
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {/* <Post author={authorName} body={bodyText} /> */}
           {posts.map((post) => (
@@ -41,10 +54,16 @@ const PostsList = ({ modalVisible, hideModal }) => {
         </ul>
       )}
 
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>No posts found</h2>
           <p>Start adding some posts </p>
+        </div>
+      )}
+
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading...</p>
         </div>
       )}
     </>

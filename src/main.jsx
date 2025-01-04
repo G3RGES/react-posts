5a77868510fe690f1c34a896e7c382e13c081192
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-01-04 17:30:48 +0200

    Update main.jsx
    CLEAN UP

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-// import App from "./App";
 import Posts, { loader as postsLoader } from "./routes/Posts";
 import "./index.css";
 import NewPost, { action as newPostAction } from "./routes/NewPost";

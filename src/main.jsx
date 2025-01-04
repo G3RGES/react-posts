40af01cd08fa4566a7385515e5b7d8266b5f37d1
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-01-04 17:49:39 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import Posts, { loader as postsLoader } from "./routes/Posts";
 import "./index.css";
 import NewPost, { action as newPostAction } from "./routes/NewPost";
 import RootLayout from "./routes/RootLayout";
-import PostDetails from "./routes/PostDetails";
+import PostDetails, { loader as postDetailsLoader } from "./routes/PostDetails";
 
 const router = createBrowserRouter([
   {
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
         loader: postsLoader,
         children: [
           { path: "create-post", element: <NewPost />, action: newPostAction },
-          { path: ":id", element: <PostDetails /> },
+          { path: "/:id", element: <PostDetails />, loader: postDetailsLoader },
         ],
       },
     ],

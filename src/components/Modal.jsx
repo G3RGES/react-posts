787e645200a25f8e1eef7c007ea39766a3087b2b
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-01-04 17:30:45 +0200

    Update Modal.jsx
    CLEAN UP

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Modal.module.css";
 import { useNavigate } from "react-router-dom";
 
-const Modal = ({ children, hideModal }) => {
+const Modal = ({ children }) => {
   const navigate = useNavigate();
 
   function closeHandler() {

864fef208ada7edaae25b16f7998bddc3fb721d8
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-01-04 15:40:20 +0200

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import classes from "./Modal.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Modal = ({ children, hideModal }) => {
+  const navigate = useNavigate();
+
+  function closeHandler() {
+    navigate("..");
+  }
+
   return (
     <>
-      <div className={classes.backdrop} onClick={hideModal} />
+      <div className={classes.backdrop} onClick={closeHandler} />
       <dialog open className={classes.modal}>
         {children}
       </dialog>

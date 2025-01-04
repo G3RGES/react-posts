5d60564035816b448e3891729fb0057e5eccdcdf
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-01-04 17:30:51 +0200

    Update Posts.jsx
    CLEAN UP

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -1,31 +1,12 @@
-import { useState } from "react";
-// import MainHeader from "./components/MainHeader";
-// import Post from "./components/Post";
-// import PostsList from "./components/PostsList";
-// import MainHeader from "../components/MainHeader";
-import Post from "../components/Post";
 import PostsList from "../components/PostsList";
 import { Outlet } from "react-router-dom";
 
 function Posts() {
-  // const [modalIsVisible, setModalIsVisible] = useState(false);
-
-  // function showModal() {
-  //   setModalIsVisible(true);
-  // }
-
-  // function hideModal() {
-  //   setModalIsVisible(false);
-  // }
-
   return (
     <>
-      {/* <MainHeader onCreatePost={showModal} /> */}
       <Outlet />
       <main>
-        <PostsList
-        //  modalVisible={modalIsVisible} hideModal={hideModal}
-        />
+        <PostsList />
       </main>
     </>
   );
